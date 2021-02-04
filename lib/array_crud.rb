b8a9cfd40a89d14e6c8fc7cec0c92e06e12644a9
Sean Brown <applebaumxyz@gmail.com>
2021-02-04 01:37:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,31 +13,38 @@ def add_element_to_end_of_array(array, element)
   element = "arrays!"
   array << element 
 end
+p add_element_to_end_of_array("array", "element")
 
 
 def add_element_to_start_of_array(array, element)
   
 end
 
+
 def remove_element_from_end_of_array(array)
   
 end
+
 
 def remove_element_from_start_of_array(array)
   
 end
 
+
 def retrieve_element_from_index(array, index_number)
   
 end
+
 
 def retrieve_first_element_from_array(array)
   
 end
 
+
 def retrieve_last_element_from_array(array)
   
 end
+
 
 def update_element_from_index(array, index_number, element)
 
