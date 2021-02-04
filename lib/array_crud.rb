6b4cba0aab342d3598c0a23da42c7babf0393e7c
Sean Brown <applebaumxyz@gmail.com>
2021-02-04 01:47:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,10 @@ p add_element_to_start_of_array("array", "element")
 
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+	array.pop
 end
+p remove_element_from_end_of_array("array")
 
 
 def remove_element_from_start_of_array(array)
