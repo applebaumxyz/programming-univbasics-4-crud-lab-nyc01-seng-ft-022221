68f28b1a1bbfcec542a055a7ed0f4d874ecad110
Sean Brown <applebaumxyz@gmail.com>
2021-02-04 01:56:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,8 +40,11 @@ p remove_element_from_start_of_array("array")
 
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 2
+  p array[2]
 end
+p retrieve_element_from_index("array", 2)
 
 
 def retrieve_first_element_from_array(array)
